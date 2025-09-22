c9e6f0a1676a9ab10ac3734398cdecdc2149f927
mahmoud <mahmoud.mahmed.salah84@gmail.com>
2025-09-23 02:22:26 +0300

    add configurations

---
+++ yourOrder.Infrastructure/Data/Configration/ProductConfigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yourOrder.Core.Entity;
+
+namespace yourOrder.Infrastructure.Data.Configration
+{
+    internal class ProductConfigration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.productBrand)
+                    .WithMany()
+                    .HasForeignKey(p => p.ProductBrandId);
+
+            builder.HasOne(p => p.productType)
+                    .WithMany()
+                    .HasForeignKey(p => p.ProductTypeId);
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(p => p.Description)
+                .IsRequired();
+            builder.Property(p => p.Price)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
---
+++ yourOrder.Infrastructure/Data/Configration/ProductTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yourOrder.Core.Entity;
+
+namespace yourOrder.Infrastructure.Data.Configration
+{
+    public class ProductTypeConfiguration : IEntityTypeConfiguration<ProductType>
+    {
+        public void Configure(EntityTypeBuilder<ProductType> builder)
+        {
+            builder.Property(pt => pt.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+    
+
+    
+}
