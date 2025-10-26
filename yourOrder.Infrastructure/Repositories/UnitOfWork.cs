f3514ec16023d1c2cb0da3fbd4ad08b65474f7e6
mahmoud <mahmoud.mahmed.salah84@gmail.com>
2025-10-26 16:09:52 +0300

    add unit of work

---
+++ yourOrder.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yourOrder.Core.Entity;
+using yourOrder.Core.Interfaces;
+using yourOrder.Infrastructure.Data;
+
+namespace yourOrder.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private Hashtable _repositories;
+        public UnitOfWork(AppDbContext context) =>_context = context;
+        
+
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose() // DI will call this method automatically to release the resource
+        {
+            _context.Dispose();
+        }
+
+        public IGenericRepository<T> Repository<T>() where T : BaseEntity
+        {
+            if (_repositories == null)
+                _repositories = new Hashtable();
+
+            var type = typeof(Product).Name; // get name of entity to use it as key in hashtable
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(T)), _context);
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IGenericRepository<T>) _repositories[type];
+        }
+    }
+}
---
+++ yourOrder.core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yourOrder.Core.Entity;
+
+namespace yourOrder.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T : BaseEntity;
+        Task<int> CompleteAsync();
+
+    }
+}
