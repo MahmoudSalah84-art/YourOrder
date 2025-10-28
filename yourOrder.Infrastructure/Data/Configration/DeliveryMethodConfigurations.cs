0a70255d855e0d5797fd9c034694a37b9ffc659d
mahmoud <mahmoud.mahmed.salah84@gmail.com>
2025-10-28 18:28:28 +0300

    add order , order item and delivey method configration

---
+++ yourOrder.Infrastructure/Data/Configration/DeliveryMethodConfigurations.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yourOrder.Core.Entity.OrderAggregate;
+
+namespace yourOrder.Infrastructure.Data.Configration
+{
+    public class DeliveryMethodConfigurations : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.Property(DeliveryMethod => DeliveryMethod.Price)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ yourOrder.Infrastructure/Data/Configration/OrderItemConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yourOrder.Core.Entity.OrderAggregate;
+
+namespace yourOrder.Infrastructure.Data.Configration
+{
+    public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.OwnsOne(item => item.ItemOrdered, product => product.WithOwner());
+
+            builder.Property(item => item.Price)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
